3be16b3e540b28adc50b83de5037cb734158fc65
gademuu <herman.dahlen@hotmail.se>
2016-10-12 19:35:26 +0200

    item view and controller

--- app/item/create-item.controller.js
+++ app/item/create-item.controller.js
@@ -1,9 +1,15 @@
 angular.module('item', [])
   .controller('CreateItemController', ['$scope', function($scope){
-    
     var vm = this;
-    vm.item = {
-      name: 'Cyckel'
-    };
+    vm.item = {};
+    vm.createItem = createItem;
 
+    function createItem(itemName, itemInfo){
+      vm.item = {
+        itemName: itemName,
+        itemInfo: itemInfo
+      };
+      alert('item created! ' + vm.item.itemName);
+
+    }
   }]);
