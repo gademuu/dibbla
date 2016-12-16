32004a09b128534ec336fc062e0671e3e2f8e456
gademuu <herman.dahlen@hotmail.se>
2016-12-16 18:54:40 +0100

    stuff

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 'use strict';
 const express = require('express');
-
 const app = express();
+
+module.exports = app;
+
+app.use(express.static(__dirname + '/app'));
 
 app.listen(8000, '0.0.0.0', function() {
   console.log("server starting on port 8000");
--- app/item/item.service.js
+++ app/item/item.service.js
@@ -7,7 +7,7 @@ angular.module('item', [])
       {title:'Cyckel', info: 'snabb och mysig'},
       {title:'Bil', info: 'inte sa snabb men den gar bra'},
   ];
-  // var items = [];
+  
     this.createItem = function(item) {
       items.push(item);
     };
