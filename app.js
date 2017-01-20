76c6aa7440ca1b59878c1ec192c67ced70723a7a
Herman Dahlén <herman.dahlen@teleopti.com>
2017-01-20 13:55:50 +0100

    minor

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-'use strict';
-const express = require('express');
-const app = express();
+'use strict'
+const express = require('express')
+const app = express()
 
-module.exports = app;
-
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/app'))
 
 app.listen(8000, '0.0.0.0', function() {
-  console.log("server starting on port 8000");
-});
+  console.log("server starting on port 8000")
+})
+
+module.exports = app
+
